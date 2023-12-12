388350a8e7b247f8c8b81b877a946ade450b7367
Dallin Olson <dallinmolson@outlook.com>
2023-12-12 01:15:01 -0800

    Update 1

--- Foundation 4/Encapsulation/Encapsulation/obj/Debug/net8.0/Encapsulation.AssemblyInfo.cs
+++ Foundation 4/Encapsulation/Encapsulation/obj/Debug/net8.0/Encapsulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encapsulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60618d42a6a7882048708f8c958af558603c3d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encapsulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encapsulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Foundation 4/Inheritance/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Foundation 4/Inheritance/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60618d42a6a7882048708f8c958af558603c3d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
